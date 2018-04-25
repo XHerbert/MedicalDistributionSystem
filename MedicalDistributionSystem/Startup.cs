aeef3f51e1987e13984c37b7c72960af6904ed6d
xuhb <421271727@qq.com>
2018-04-25 18:09:42 +0800

    添加项目文件。

---
+++ MedicalDistributionSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedicalDistributionSystem.Startup))]
+namespace MedicalDistributionSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
